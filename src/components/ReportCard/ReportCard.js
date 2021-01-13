b33566977f24b9bb1e9383b84f69355b2a75cc5e
Zach Goodrich <zachery.goodrich@gmail.com>
2021-01-13 14:15:09 -0700

    styled finish time

--- src/components/ReportCard/ReportCard.js
+++ src/components/ReportCard/ReportCard.js
@@ -7,12 +7,12 @@ const ReportCard = (props) => {
         <div className='col-10 reportCardCol mt-2'>
             <div className='card h-100'>
                 <div className='card-body d-flex flex-column justify-content-center align-items-center'>
-                    <div className='h-25 w-100 bg-success d-flex justify-content-center align-items-center'>
-                        <p className='cardTitle card-title'>Great Job!</p>
+                    <div className='h-25 w-100 d-flex justify-content-center align-items-center'>
+                        <p className='cardTitle card-title text-primary'>Great Job!</p>
                     </div>
-                    <div className='card-body bg-warning w-100 d-flex flex-column align-items-center pt-5'>
+                    <div className='card-body w-100 d-flex flex-column align-items-center'>
                         <p className='card-text mb-4'>You finished in:</p>
-                        <p className='card-text display-4'>{props.finishTime}</p>
+                        <p className='card-text flex-grow-1 d-flex align-items-center justify-content-center reportCardTimeText'>{props.finishTime}</p>
                     </div>
                 </div>
             </div>
